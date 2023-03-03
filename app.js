b6b6d0ec0385801b759dcc8fcf5f192567429f3e
curranod <curranod840@gmail.com>
2023-03-03 15:23:10 -0600

    final updates for friday

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const cardtitle = document.getElementById("card-title");
 const ul = document.getElementById("ul");
 const additional = document.getElementById("additional");
 
-function display () {
-fetch("https://overfast-api.tekrop.fr/heroes")
-  .then((response) => response.json())
-  .then((information) => {
-    // information.map((hero_key) => additionalInfo(hero_key))
-    
-    const heroList = information.map(function (stuff) {
-      return `
+function display() {
+  fetch("https://overfast-api.tekrop.fr/heroes")
+    .then((response) => response.json())
+    .then((information) => {
+     const heroList = information.map(function (stuff) {
+        return `
       <a onclick='additionalInfo("${stuff.name}")'>
       <div id="card">
       <img src="${stuff.portrait}" class = "card" id="card-img-top" alt="...">
@@ -22,33 +20,28 @@ fetch("https://overfast-api.tekrop.fr/heroes")
         </a>
       </div>
     </div>`;
+      });
+      body.innerHTML = heroList.join("");
     });
-    body.innerHTML = heroList.join("");
-});
 }
-display ()
+display();
+
 function additionalInfo(heroName) {
-  // let heroName = "${stuff.name}"
-  fetch(`https://overfast-api.tekrop.fr/heroes/${heroName}`, {
-    method: 'GET'
+  fetch(`https://overfast-api.tekrop.fr/heroes/${heroName.toLowerCase()}`, {
+    method: "GET",
   })
     .then((response) => response.json())
     .then((information) => {
-      const heroInfo = information.map(function (info) {
-        return `<li>
-        <b>Name: ${info.name}</b>
-        <p>About: ${info.description}</p>
-        <img src=${info.portrait}>
-        <h2>Role: ${info.role}</h2>
-        <p>Location: ${info.location}</p>
-        <p>Abilities: ${info.abilities}</p>
-        <p>Origin Story: ${info.story}</p>
+      const heroList = `<li>
+        <b>Name: ${information.name}</b>
+        <p>About: ${information.description}</p>
+        <img src=${information.portrait}>
+        <h2>Role: ${information.role}</h2>
+        <p>Location: ${information.location}</p>
+        <p>Origin Story: ${information.story.summary}</p>
 </li>`;
-      });
-      additional.innerHTML += heroInfo;
-    });
-}
 
-function test () {
-  console.log(`stuff.hero.key`)
+      additional.innerHTML = heroList;
+      console.log(heroList);
+    });
 }
