305a1b8a2b98538a77f7270e9ee9eca2d32b9dcc
root <root@DESKTOP-FMI1SH6>
2023-03-03 11:04:48 -0500

    changes

---
+++ app.js
@@ -0,0 +1,15 @@
+const ul = document.getElementById("ul");
+
+fetch("https://overfast-api.tekrop.fr/heroes")
+  .then((response) => response.json())
+  .then((information) => {
+    console.log(information);
+    const heroList = information.map(function (stuff) {
+      return `<li>
+        <div>
+        ${stuff.name}</div>
+        <div>${stuff.role}</div>
+        <img src="${stuff.portrait}"></li>`;
+    });
+    ul.innerHTML = heroList;
+  });
